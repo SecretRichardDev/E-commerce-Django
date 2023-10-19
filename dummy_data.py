9977aa31022c24ee415d363e352c20eddfdf89c7
SecretRichardDev <secretdev930817@gmail.com>
2023-10-19 06:30:10 +0300

    add product image by dummy_data

--- dummy_data.py
+++ dummy_data.py
@@ -48,23 +48,26 @@ def seed_product(n):
     print(f"Seed {n} Products Successfully")
 
 
+
 def seed_product_images(n):
     fake = Faker()
     images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg', '11.jpg', '12.jpg', '13.jpg', '14.jpg', '15.jpg', '16.jpg', '17.jpg', '18.jpg', '19.jpg', '20.jpg', '21.jpg']
     for _ in range(n):
         ProductImages.objects.create(
-            product = Product.objects.get(id=random.randint(1,1020)),
-            image = f'product_images/{images[random.randint(0,20)]}'
+            product = Product.objects.get(id=random.randint(43,1040)),
+            image = f'images_proudct/{images[random.randint(0,20)]}',
         )
 
     print(f"Seed {n} images in product Successfully")
+
+
 
 
 def seed_reviews(n):
     fake = Faker()
     for _ in range(n):
         Reviews.objects.create(
-            product = Product.objects.get(id=random.randint(1,1100)),
+            product = Product.objects.get(id=random.randint(1, 1040)),
             rate = random.randint(0,4) , 
             review = fake.text(max_nb_chars=250), 
         )
