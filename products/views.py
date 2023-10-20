e64da95150f7595c1753ed4c730bfc6758a6529e
SecretRichardDev <secretdev930817@gmail.com>
2023-10-20 11:09:58 +0300

    add counter review in product_detail

--- products/views.py
+++ products/views.py
@@ -14,5 +14,11 @@ class ProductDetail(DetailView):
     def get_context_data(self, **kwargs): 
         context =  super().get_context_data(**kwargs)
         # context["reviews"] = Review.objects.filter(product=self.get_object())
+        
+        # code for  test 
+        # كدا هات كل المنجات اللي الحجم بتاعها زي المنتج بتاعي دلوقتي
+        # context['ralated_size'] = Product.objects.filter(size = self.get_object().size)
+        
+        # دي كدا معناها هات المنتجات اللي الكاتيجري بتاعها موجود في المنج بتاعنا دلوقتي 
         context["related_products"] = Product.objects.filter(categore=self.get_object().categore)
         return context
