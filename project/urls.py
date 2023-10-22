adaa694aaf6f4866b9cb88aca4f77856793ac9fc
SecretRichardDev <secretdev930817@gmail.com>
2023-10-22 07:12:41 +0300

    add rest_framework in my project

--- project/urls.py
+++ project/urls.py
@@ -23,7 +23,11 @@ urlpatterns = [
     
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+
+
+
     path('__debug__/', include(debug_toolbar.urls)),
+    path('api-auth/', include('rest_framework.urls')),
 
 ]
 
