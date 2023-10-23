d94b2db64b05ebd0ddbdfb8444e1675c3adc89a2
SecretRichardDev <secretdev930817@gmail.com>
2023-10-23 15:30:01 +0300

    add rating in product_list

--- setting/views.py
+++ setting/views.py
@@ -5,5 +5,6 @@ from products.models import Product, Categories
 def index(request):
     all_product = Product.objects.all()
     categories = Categories.objects.all()[:5]
-     
     return render(request, 'setting/index.html', {'all_product':all_product, 'categories':categories})
+
+
