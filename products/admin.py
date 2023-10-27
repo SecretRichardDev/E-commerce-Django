358aa588b80e946e6333c23ce5d7bd7c5ce67f6f
SecretRichardDev <secretdev930817@gmail.com>
2023-10-27 13:12:00 +0200

    test drow.io in my project

--- products/admin.py
+++ products/admin.py
@@ -2,7 +2,14 @@ from django.contrib import admin
 from .models import Product, Categories, ProductImages, Reviews
 # Register your models here.
 
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_filter = ["categore"]
+    search_fields = ["name"]
+
+
 admin.site.register(Categories)
-admin.site.register(Product)
+admin.site.register(Product, PersonAdmin)
 admin.site.register(ProductImages)
-admin.site.register(Reviews)
+admin.site.register(Reviews)    
